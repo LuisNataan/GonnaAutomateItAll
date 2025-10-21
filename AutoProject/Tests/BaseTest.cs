3f3f944c54134073d9e057e249df5ef5b2b48387
LuisNataan <luisnataan556@gmail.com>
2025-10-21 13:19:01 -0300

    BaseTests configurations for Web browser added,

--- AutoProject/Tests/BaseTest.cs
+++ AutoProject/Tests/BaseTest.cs
@@ -15,8 +15,23 @@ namespace AutoProject.Tests
         public virtual void SetUp()
         {
             var opt = new ChromeOptions();
+            
+            //Arguments
             opt.AddArgument("--start-maximized");
             opt.AddArgument("--disable-notifications");
+            opt.AddArgument("--incognito");
+            opt.AddArgument("--no-default-browser-check");
+            opt.AddArgument("--no-first-run");
+            opt.AddArgument("--diasble-blink-fetrures=AutomationControlled");
+            
+
+            
+            opt.AddUserProfilePreference("credential_enable_service", false);
+            opt.AddUserProfilePreference("profile.password_manager_enabled", false);
+            opt.AddUserProfilePreference("credentials_enable_service", false);
+            opt.AddUserProfilePreference("profile.default_content_setting_values.notifications", 2);
+
+            opt.AddExcludedArgument("enable-automation");
 
             Driver = new ChromeDriver(opt);
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
